8eea3d8b56bc1a15615d5373f06584652f28a0ef
dkiryanov <dkiryanov@fix.ru>
2020-05-16 01:25:35 +0300

    meet the Django with easy article and comments realisation

--- promo_life_is_good/apps/main/views.py
+++ promo_life_is_good/apps/main/views.py
@@ -1,3 +1,35 @@
+# from django.http import HttpResponse
+#
+#
+# def index(request):
+#     return HttpResponse('Привет, мир!')
+
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+from django.urls import reverse
+
+from .models import Article
+
+
+def index(request):
+    latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+    return render(request, 'main/list.html', {'latest_articles_list': latest_articles_list})
+
+
+def detail(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статья не найдена!")
+    latest_comments_list = a.comment_set.order_by('-id')[:10]
+    return render(request, 'main/detail.html', {'article': a, 'latest_comments_list': latest_comments_list})
+
+
+def leave_comment(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статья не найдена!")
+    a.comment_set.create(author_name=request.POST['name'], comment_text=request.POST['text'])
+    return HttpResponseRedirect(reverse('main:detail', args=(a.id,)))
