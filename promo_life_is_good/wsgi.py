293b1ac4148f5e58cd1fef41d2b4992d0ddd0ead
dkiryanov <dkiryanov@fix.ru>
2020-05-15 20:06:19 +0300

    initial commit

---
+++ promo_life_is_good/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for promo_life_is_good project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'promo_life_is_good.settings')
+
+application = get_wsgi_application()
